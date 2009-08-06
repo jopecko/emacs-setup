1ed13d3e4066a373febe15d4e3b815199479d3e8
Joe O'Pecko <opeckojo@gmail.com>
2009-08-05 21:49:32 -0400

    updated clojure-related configuration to coincide with latest development stream

--- ini/002-slime.el
+++ ini/002-slime.el
@@ -7,19 +7,9 @@
 ;;____________________________________________________________________
 ;;;;    Programming - Slime
 
-(setq slime-dir (concat home-dir "lisp/slime"))
 (when (file-directory-p slime-dir)
   (add-to-list 'load-path slime-dir)
+  (add-to-list 'load-path (concat slime-dir "/contrib"))
+
   (require 'slime)
-  (slime-setup))
-
-;; jump to slime when a lisp file is opened
-;(add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
-;(add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
-
-;(setq inferior-lisp-program ""
-;      lisp-indent-function 'common-lisp-indent-function
-;      slime-complete-symbol-function 'slime-fuzzy-complete-symbol
-;      common-lisp-hyperspec-root ""
-;      slime-startup-animation t)
-
+  (slime-setup '(slime-fancy)))
--- ini/clojure.el
+++ ini/clojure.el
@@ -7,66 +7,18 @@
 ;;____________________________________________________________________
 ;;;;    Programming - Clojure
 
-(setq clj-dir (concat home-dir "lisp/clj"))
+;; dump message into *msg* buffer to remind to setq
 
-(setq clojure-mode-dir (concat clj-dir "clojure-mode"))
-(when (file-directory-p clojure-mode-dir)
-  (add-to-list 'load-path clojure-mode-dir)
+(when (file-directory-p clj-dir)
+  (add-to-list 'load-path (concat clj-dir "/clojure-mode"))
+  (add-to-list 'load-path (concat clj-dir "/swank-clojure"))
+
   (autoload 'clojure-mode "clojure-mode" "A major mode for Clojure" t)
   (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-  ;;(require 'clojure-auto)
-)
+  ;(setq swank-clojure-binary (concat bin-dir "clj"))
+  (setq swank-clojure-jar-path (concat clj-dir "/clojure/clojure.jar"))
+  (require 'swank-clojure-autoload)
 
-;; swank-clojure
-(setq swank-clojure-dir (concat clj-dir "swank-clojure"))
-(when (file-directory-p swank-clojure-dir)
-  (add-to-list 'load-path swank-clojure-dir)
-  (setq swank-clojure-binary (concat home-dir "bin/clj"))
-  (require 'swank-clojure-autoload))
-
-;(defvar clj-root (concat home-dir "lisp/clj"))
-;(setq load-path (append (list (concat clj-root "slime")
-;			      (concat clj-root "slime/contrib")
-;			      (concat clj-root "clojure-mode")
-;			      (concat clj-root "swank-clojure"))
-;			load-path))
-
-;(setq swank-clojure-binary "clj")
-;(defvar clj-cmd)
-;(setenv "CLJ_CMD"
-;	(setq clj-cmd
-;	      (concat "java "
-;		      "-server "
-;		      "-Xdebug -runjdwp:transport=dt_socket,server=y,suspend=n,address=8888 "
-;		      "clojure.lang.Repl")))
-
-
-
-;;; Jim Weirich's setup
-;(setq inferior-lisp-program
-;                                        ; Path to java implementation
-;      (let* ((java-path "java")
-                                        ; Extra command-line options
-                                        ; to java.
-;             (java-options "-Xms100M -Xmx600M")
-                                        ; Base directory to Clojure.
-                                        ; Change this accordingly.
-;             (clojure-path "/usr/local/clojure/")
-                                        ; The character between
-                                        ; elements of your classpath.
-;             (class-path-delimiter ":")
-;             (class-path (mapconcat (lambda (s) s)
-                                        ; Add other paths to this list
-                                        ; if you want to have other
-                                        ; things in your classpath.
-;                                    (list (concat clojure-path "clojure.jar")
-;       (concat clojure-path "clojure-contrib.jar"))
-;                                    class-path-delimiter)))
-;        (concat java-path
-;                " " java-options
-;                " -cp " class-path
-;                " clojure.lang.Repl")))
- 
-;; Require clojure-mode to load and associate it to all .clj files.
-
-;(require 'clojure-mode)
+  (unless (boundp 'swank-clojure-extra-classpaths)
+    (setq swank-clojure-extra-classpaths nil))
+  (add-to-list 'swank-clojure-extra-classpaths (concat clj-dir "/clojure-contrib/src")))
