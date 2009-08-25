9720fdae7210856b6c9651594b41f2a59e05f2d0
Joe O'Pecko <opeckojo@gmail.com>
2009-08-24 22:42:49 -0400

    updated clojure configuration

--- ini/002-slime.el
+++ ini/002-slime.el
@@ -12,4 +12,22 @@
   (add-to-list 'load-path (concat slime-dir "/contrib"))
 
   (require 'slime)
-  (slime-setup '(slime-fancy)))
+  (slime-setup '(slime-fancy))
+
+  (defun slime-smart-quit ()
+    (interactive)
+    (when (slime-connected-p)
+      (if (equal (slime-machine-instance) "my.workstation")
+          (slime-quit-lisp)
+        (slime-disconnect)))
+    (slime-kill-all-buffers))
+
+  ;(define-key slime-repl-mode-map [(hyper q)] 'slime-smart-quit)
+
+  (add-hook 'kill-emacs-hook 'slime-smart-quit))
+
+;; need to find a home for this
+(require 'paredit)
+(defun lisp-enable-paredit-hook () (paredit-mode 1))
+;; enable paredit in slime repl
+(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
--- ini/clojure.el
+++ ini/clojure.el
@@ -15,10 +15,54 @@
 
   (autoload 'clojure-mode "clojure-mode" "A major mode for Clojure" t)
   (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-  ;(setq swank-clojure-binary (concat bin-dir "clj"))
   (setq swank-clojure-jar-path (concat clj-dir "/clojure/clojure.jar"))
   (require 'swank-clojure-autoload)
 
   (unless (boundp 'swank-clojure-extra-classpaths)
     (setq swank-clojure-extra-classpaths nil))
-  (add-to-list 'swank-clojure-extra-classpaths (concat clj-dir "/clojure-contrib/src")))
+  (add-to-list 'swank-clojure-extra-classpaths (concat clj-dir "/clojure-contrib/src"))
+
+  ;; provides support for running Clojure tests from within Emacs buffers
+  ;; and seeing the results displayed inline.
+  (autoload 'clojure-test-mode "clojure-test-mode" "Clojure test mode" t)
+  (autoload 'clojure-test-maybe-enable "clojure-test-mode" "" t)
+  (add-hook 'clojure-mode-hook 'clojure-test-maybe-enable)
+  (add-hook 'clojure-mode-hook
+            (lambda () (save-excursion
+                         (goto-char (point-min))
+                         (if (search-forward "(deftest" nil t)
+                             (clojure-test-mode)))))
+
+  ;; Clojure Paredit integration
+  (add-hook 'clojure-mode-hook 'lisp-enable-paredit-hook)
+
+  ;; http://technomancy.us/126
+  ;; prompt for a project root and then launch a SLIME session configured with
+  ;; a classpath for that project
+  (defun clojure-project (path)
+    "Setup classpaths for a clojure project and starts a new SLIME session."
+    (interactive (list
+                  (ido-read-directory-name
+                   "Project root: "
+                   (locate-dominating-file default-directory "pom.xml"))))
+    (when (get-buffer "*inferior-lisp*")
+      (kill-buffer "*inferior-lisp*"))
+    (defvar swank-clojure-extra-vm-args nil)
+    (defvar slime-lisp-implementations nil)
+    (add-to-list 'swank-clojure-extra-vm-args
+                 (format "-Dclojure.compile.path=%s"
+                         (expand-file-name "target/classes/" path)))
+    (setq swank-clojure-binary nil
+          swank-clojure-jar-path (expand-file-name "target/dependency/" path)
+          swank-clojure-extra-classpaths
+          (append (mapcar (lambda (d) (expand-file-name d path))
+                          '("src/" "target/classes/" "test/"))
+                  (let ((lib (expand-file-name "lib" path)))
+                    (if (file-exists-p lib)
+                        (directory-files lib t ".jar$"))))
+          slime-lisp-implementations
+          (cons `(clojure ,(swank-clojure-cmd) :init swank-clojure-init)
+                (remove-if #'(lambda (x) (eq (car x) 'clojure))
+                           slime-lisp-implementations)))
+    (save-window-excursion
+      (slime))))
